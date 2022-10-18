055ee066fbfc557d4b5af05cd30cc2b31b298536
Hasnain01-hub <hasnainsayyed833@gmail.com>
2022-10-18 13:38:33 +0530

    Place Order done

--- client/app/reducers/cartReducer.js
+++ client/app/reducers/cartReducer.js
@@ -2,18 +2,20 @@ let initialState = [];
 
 // load cart items from local storage
 if (typeof window !== "undefined") {
-    if (localStorage.getItem("cart")) {
-        initialState = JSON.parse(localStorage.getItem("cart"));
-    } else {
-        initialState = [];
-    }
+  if (localStorage.getItem("cart")) {
+    initialState = JSON.parse(localStorage.getItem("cart"));
+  } else {
+    initialState = [];
+  }
 }
 
 export const cartReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case "ADD_TO_CART":
-            return action.payload;
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case "ADD_TO_CART":
+      return action.payload;
+    case "REMOVE_FROM_CART":    
+      return action.payload;
+    default:
+      return state;
+  }
 };
--- client/app/routes/checkout.js
+++ client/app/routes/checkout.js
@@ -13,15 +13,24 @@ const checkout = ({ history }) => {
     // return cart.reduce((currentValue, nextValue) => {
     //     return currentValue + nextValue.count * nextValue.price;
     // }, 0);
-    var sum =0;
+    var sum = 0;
     cart.map((item) => {
-      var values=item.variants[0]['prices'][0]['amount']*item.count;
-        sum +=values ;
-      console.log(item.variants[0]['prices'][0]['amount']);
+      var values = item.variants[0]["prices"][0]["amount"] * item.count;
+      sum += values;
+      // console.log(item.variants[0]["prices"][0]["amount"]);
     });
-    return sum/ 100;
+    return sum / 100;
   };
-
+  const placeorder = () => {
+    if (confirm("Are you sure you want to place order?")) {
+      
+      localStorage.setItem("cart", JSON.stringify([]));
+      dispatch({
+        type: "REMOVE_FROM_CART",
+        payload: [],
+      });
+    }
+  };
   const showCartItems = () => (
     <table className="table table-bordered">
       <thead className="thead-light">
@@ -32,6 +41,7 @@ const checkout = ({ history }) => {
           <th scope="col">Brand</th>
           <th scope="col">Count</th>
           <th scope="col">Remove</th>
+          <th scope="col"></th>
         </tr>
       </thead>
 
@@ -43,63 +53,45 @@ const checkout = ({ history }) => {
 
   return (
     <>
-    
-    <div
-      className="container-fluid pt-2"
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-    >
-      <div className="row">
-        <div className="col-md-8">
-          <h4>Cart / {cart.length} Products</h4>
+    {console.log(cart[0])}
+      <div
+        className="container-fluid pt-2"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+      >
+        <div className="row">
+          <div className="col-md-8">
+            <h4>Cart / {cart.length} Products</h4>
 
-          {!cart.length ? (
-            <p>
-              No services in cart.{" "}
-              <Link to="/allservices">Continue Shopping.</Link>
-            </p>
-          ) : (
-            showCartItems()
-          )}
+            {!cart.length ? (
+              <p>
+                No services in cart.{" "}
+                <Link to="/allservices">Continue Shopping.</Link>
+              </p>
+            ) : (
+              showCartItems()
+            )}
+          </div>
+          <div className="col-md-4">
+            <h4>Summary</h4>
+            <hr />
+            Total: <b>€.{getTotal()}</b>
+            <hr />
+          </div>
         </div>
-        <div className="col-md-4">
-          <h4>Summary</h4>
-          <hr />
-          Total: <b>€.{getTotal()}</b>
-          <hr />
-          {user ? (
-            <>
-              {/* <button
-                                onClick={saveOrderToDb}
-                                className="btn btn-sm btn-primary mt-2"
-                                disabled={!cart.length}
-                            >
-                                Proceed to Checkout
-                            </button> */}
-              <br />
-              <button
-                className="btn btn-sm btn-primary mt-2"
-                disabled={!cart.length}
-              >
-                Pay Cash on Service Completion
-              </button>
-            </>
-          ) : (
-            <button className="btn btn-sm btn-primary mt-2">
-              <Link
-                to={{
-                  pathname: "/login",
-                  state: { from: "cart" },
-                }}
-              >
-                Checkout
-              </Link>
-            </button>
-          )}
-        </div>
+        {cart.length>0?<button
+          style={{
+            border: "1px solid black",
+            padding: "10px",
+            borderRadius: "15px",
+            margin: "10px 0",
+          }}
+          onClick={placeorder}
+        >
+          Place Order
+        </button>:<></>}
       </div>
-    </div>
     </>
   );
 };
